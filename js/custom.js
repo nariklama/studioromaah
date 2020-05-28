655f1e0f8a3130e77a18a6b06795139da8dfe990
Amal Kiran <amal.kiran13@gmail.com>
2020-05-28 17:06:15 +0530

    First commit

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+/*
+Template Name: beauticastle
+File Name: custom.js
+Author Name: ThemeVault
+Author URI: http://www.themevault.net/
+License URI: http://www.themevault.net/license/
+*/
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
